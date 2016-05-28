dc6960cc05be869d32dcdccb549c2b2dca0ec380
Eduardo fernandez <eduardo.fernandez4@capitalone.com>
2016-05-28 13:31:00 -0400

    Update 2016-05-28T17:30:59.690Z

--- main.js
+++ main.js
@@ -35483,13 +35483,13 @@ angular
     .module('BWPestSolutions.home')
     .config(function ($stateProvider, $urlRouterProvider, $locationProvider) {
 
-      $urlRouterProvider.otherwise('/home');
+      $urlRouterProvider.otherwise('home');
         
       $stateProvider
             
             .state('home', {
               url: '/home',
-              templateUrl: '/views/home.html',
+              templateUrl: './views/home.html',
             });
 
       // $locationProvider.html5Mode(true); 
